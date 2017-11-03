c051f8632c173f7ce5861bce5d3b3430bcf4ee9a
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-11-03 10:58:16 -0400

    Radians and Degrees

--- C#/calculation.cs
+++ C#/calculation.cs
@@ -11,21 +11,26 @@ public class Calc
       {
             //Latitude coordinates
             double lat1 = 32.828819;
-            double lat2 = 29.951067;
+            double lat2 = 29.951061;
             //Longitude coordinates
             double longit1 = -83.649777;
-            double longit2 = -90.081260;
+            double longit2 = -90.081244;
             //Delta lat. and delta longit.
             double dlat = lat2 - lat1;
             double dlongit = longit2 - longit1;
             //Given equitorial radius constant in miles
             double R = 3959.999;
-            double temp = Math.Pow(Math.Sin(dlat / 2), 2) + (Math.Cos(lat1) * Math.Cos(lat2) * Math.Pow(Math.Sin(dlongit / 2), 2));
+            double temp = Math.Pow((Math.Sin((dlat / 2)) * (180.0 / Math.PI)), 2) + ((Math.Cos(lat1) * (180.0 / Math.PI)) * 
+                          (Math.Cos(lat2) * (180.0 / Math.PI)) * Math.Pow((Math.Sin(dlongit / 2) * (180.0 / Math.PI)), 2));
+            double degrees = Math.Asin(Math.Sqrt(temp)); //* (180.0 / Math.PI);
             //Calculates the distance between geographic coordinates
-            double d = 2 * R * Math.Asin(Math.Sqrt(temp));
+            double d = 2 * R * degrees;
+            Console.WriteLine("Might Need to convert to Degrees not Radians...");
+            Console.WriteLine(Math.Pow(Math.Sin(dlat/2),2));
             Console.WriteLine("delta Lat = " + dlat);
             Console.WriteLine("delta Long = " + dlongit);
             Console.WriteLine("Temp = " + temp);
             Console.WriteLine("Distance = " + d);
+            Console.WriteLine("Distance should be appr. 428.09 miles");
       }
 }
