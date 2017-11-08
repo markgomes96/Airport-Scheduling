dd27b9b3342fe2e20684d528fdddf23a2a39233d
Avery Zebell <averyzebell@gmail.com>
2017-11-08 10:28:02 -0500

    Perl: implemented web scraper

--- perl/reader.pl
+++ perl/reader.pl
@@ -1,3 +1,13 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+
+# Library for WWW in Perl (LWP)
+use LWP::Simple;
+
+my $url = "http://theochem.mercer.edu/csc330/data/airports.csv";
+my $content = get($url);
+print($content);
+
 
