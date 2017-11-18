2e81d96a31ae3a041e468794a16c2b1328f3e749
Avery Zebell <averyzebell@gmail.com>
2017-11-18 16:44:05 -0500

    minor syntax corrections

--- C#/airplane.cs
+++ C#/airplane.cs
@@ -9,21 +9,21 @@ public class Airplane
   public double Miles_Before_Refill {get; set;}
   public int Runway_Len {get; set;}
   public string[] Surfaces {get; set;}
-  public int Passengers {get; set;}
+  public int Num_Passengers {get; set;}
   public int Capacity {get; set;}
   public string[] Passengers {get; set;}
   
-  public Airplane(int speed, double miles_flown, double miles_before_refill, int runway_len, string[] surfaces, int passengers, int capacity, string[] passengers)
+  public Airplane(int speed, double miles_flown, double miles_before_refill, int runway_len, string[] surfaces, int num_passengers, int capacity, string[] passengers)
   {
   
-    int Speed = speed; //cruising speed of plane in mph
-    double Miles_Flown = miles_flown; //miles flown in a day
-    double Miles_Before_Refill = miles_before_refill; //miles left before plane needs to refill
-    int Runway_Len = runway_len; //length of runway needed (minimum)
-    string[] Surfaces = surfaces; //array of surfaces plane can land on
-    int Passengers = passengers; //number of passengers on board
-    int Capacity = capacity; //maximum amount of passengers
-    string[] Passengers = passengers; //array of passengers currently on board
+    this.Speed = speed; //cruising speed of plane in mph
+    this.Miles_Flown = miles_flown; //miles flown in a day
+    this.Miles_Before_Refill = miles_before_refill; //miles left before plane needs to refill
+    this.Runway_Len = runway_len; //length of runway needed (minimum)
+    this.Surfaces = surfaces; //array of surfaces plane can land on
+    this.Num_Passengers = num_passengers; //number of passengers on board
+    this.Capacity = capacity; //maximum amount of passengers
+    this.Passengers = passengers; //array of passengers currently on board
   
   }
 
---
+++ C#/main.cs
@@ -0,0 +1,20 @@
+
+// Referenced:
+//     https://stackoverflow.com/questions/10826260/is-there-a-way-to-read-from-a-website-one-line-at-a-time
+//
+
+using System;
+using System.IO;
+using System.Net;
+
+
+public class reader {
+	static public void Main() {
+
+		Airplane a = new Airplane(500, 0, 3000, 3000, new[] {""}, 0, 10, new[] {""});
+
+		Console.WriteLine(a.Speed);
+
+	}
+}
+
