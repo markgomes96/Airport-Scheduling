9b334e3336a39262ca7e8e306263cb8e355383cb
Avery Zebell <averyzebell@gmail.com>
2017-11-28 19:04:15 -0500

    Fixed incomplete merge

--- C#/Geolocator.cs
+++ C#/Geolocator.cs
@@ -8,139 +8,128 @@ using System;
  *--------------------------------------------------------*/
 public class Geolocator {
 
-    /*---------------------------------------------------------
-     * Method: getDistance
-     *
-     * Purpose: Calculates the distance between two given coords
-     *
-     * Returns: A 'double' value of the distance
-     *--------------------------------------------------------*/
-    public static double getDistance(double lat1, double long1, double lat2, double long2) {
-        //
-        // TODO:
-        //   Implement this method using the equation 
-        //   given to us by Dr. Pounds.
-        //   Refer to 'calculation.cs' for a working demo
-        //
+	/*---------------------------------------------------------
+	 * Method: getDistance
+	 *
+	 * Purpose: Calculates the distance between two given coords
+	 *
+	 * Returns: A 'double' value of the distance
+	 *--------------------------------------------------------*/
+	public static double getDistance(double lat1, double long1, double lat2, double long2) {
 
-        // delta lat and delta longit
-        double dlat = lat2 - lat1;
-        double dlongit = long2 - long1;
-        // Equitorial radius constant in miles
-        double R = 3959.999;
-        // Convert to radians
-        double temp = Math.Pow((Math.Sin((dlat / 2) * (Math.PI / 180.0))), 2) + ((Math.Cos(lat1 * (Math.PI / 180.0))) * 
-                (Math.Cos(lat2 * (Math.PI / 180.0))) * Math.Pow((Math.Sin((dlongit / 2) * (Math.PI / 180.0))), 2));
-        double radian = Math.Asin(Math.Sqrt(temp));
-        //Calculates the distance between geographic coordinates
-        double d = 2 * R * radian;
-        return d;
-    }
+		// delta lat and delta longit
+		double dlat = lat2 - lat1;
+		double dlongit = long2 - long1;
+		// Equitorial radius constant in miles
+		double R = 3959.999;
+		// Convert to radians
+		double temp = Math.Pow((Math.Sin((dlat / 2) * (Math.PI / 180.0))), 2) + ((Math.Cos(lat1 * (Math.PI / 180.0))) * 
+				(Math.Cos(lat2 * (Math.PI / 180.0))) * Math.Pow((Math.Sin((dlongit / 2) * (Math.PI / 180.0))), 2));
+		double radian = Math.Asin(Math.Sqrt(temp));
+		//Calculates the distance between geographic coordinates
+		double d = 2 * R * radian;
+		return d;
+	}
 
 
-    /*---------------------------------------------------------
-     * Method: findCoords
-     * 
-     * Purpose: Finds the averaged coordinates of a city
-     *          using the .csv file containing coordinates 
-     *          for various zip codes.
-     *
-     * Parameters: 
-     *   city - a string containing the name of the city
-     *   statecode - the 2-letter abbreviation for the state
-     * 
-     * Returns: A string with the lat and long separated
-     *          by a comma.
-     *          Returns '-1, -1' if the city is not found.
-     *--------------------------------------------------------*/
-    public static string findCoords(string city, string statecode) {
+	/*---------------------------------------------------------
+	 * Method: findCoords
+	 * 
+	 * Purpose: Finds the averaged coordinates of a city
+	 *          using the .csv file containing coordinates 
+	 *          for various zip codes.
+	 *
+	 * Parameters: 
+	 *   city - a string containing the name of the city
+	 *   statecode - the 2-letter abbreviation for the state
+	 * 
+	 * Returns: A string with the lat and long separated
+	 *          by a comma.
+	 *          Returns '-1, -1' if the city is not found.
+	 *--------------------------------------------------------*/
+	public static string findCoords(string city, string statecode) {
 
-        double latSum = 0.0;
-        double longSum = 0.0;
-        int count = 0;
+		double latSum = 0.0;
+		double longSum = 0.0;
+		int count = 0;
 
-        // read through the .csv file
-        // and sum the coordinates where the
-        // city and statenames match
-        string url = "http://theochem.mercer.edu/csc330/data/zip_codes_states.csv";
-        WebReader reader = new WebReader(url);
-        string line = reader.getLine();
-        while(line != null) {
-            string[] columns = line.Split(',');
-            if(columns[3].Contains(city) && columns[4].Contains(statecode)) {
-                latSum += Double.Parse(columns[1]);
-                longSum += Double.Parse(columns[2]);
-                count++;
-            }
-            line = reader.getLine();
-        }
+		// read through the .csv file
+		// and sum the coordinates where the
+		// city and statenames match
+		string url = "http://theochem.mercer.edu/csc330/data/zip_codes_states.csv";
+		WebReader reader = new WebReader(url);
+		string line = reader.getLine();
+		while(line != null) {
+			string[] columns = line.Split(',');
+			if(columns[3].Contains(city) && columns[4].Contains(statecode)) {
+				latSum += Double.Parse(columns[1]);
+				longSum += Double.Parse(columns[2]);
+				count++;
+			}
+			line = reader.getLine();
+		}
 
-<<<<<<< HEAD
-        if(count==0) 
-            return "-1, -1";
-        return (latSum/count) + ", " + (longSum/count);
-    }
-=======
 		if(count==0) 
 			return "-1, -1";
 		return (latSum/count) + ", " + (longSum/count);
 	}
-  public static List<Airport> CreateAirportDatabase()      //creates a database of all usable airports
-  {
-    List<Airport> apdb = new List<Airport>();    //list of airport objects to store all usable airports
-       
-    string url_airports = "http://theochem.mercer.edu/csc330/data/airports.csv";
+
+
+	public static List<Airport> CreateAirportDatabase()      //creates a database of all usable airports
+	{
+		List<Airport> apdb = new List<Airport>();    //list of airport objects to store all usable airports
+
+		string url_airports = "http://theochem.mercer.edu/csc330/data/airports.csv";
 		WebReader wr = new WebReader(url_airports);              //reads in the airport file
 		string line = wr.getLine();
-    String[] apdata;
-    
-    while(line != null)       //iterates through the airport file
-    {
-      line = line.Replace('"',' ');        //manipulates the data by splitting it up into an array
-      apdata = line.Split(',');
-      //Goes through multiple checks so only relavent data is processed
-      if(apdata[8].Trim().Equals("US", StringComparison.Ordinal) || apdata[8].Trim().Equals("PR", StringComparison.Ordinal) || apdata[8].Trim().Equals("AS", StringComparison.Ordinal))
-      {
-        if(apdata[2].Trim().Equals("small_airport", StringComparison.Ordinal) || apdata[2].Trim().Equals("large_airport", StringComparison.Ordinal))
-        {
-          if(apdata[11].Trim().Equals("no", StringComparison.Ordinal))
-          {
-            //adds a new airport object with all relevant data from airport file
-            apdb.Add(new Airport(Int32.Parse(apdata[0].Trim()), apdata[3].Trim(), Convert.ToDouble(apdata[4].Trim()), Convert.ToDouble(apdata[5].Trim()), 
-                    apdata[8].Trim(), apdata[9].Substring(4,2), apdata[10].Trim(), apdata[12].Trim(), 0, " ", 0));
-          }
-        }
-      }
-		  line = wr.getLine();
+		String[] apdata;
+
+		while(line != null)       //iterates through the airport file
+		{
+			line = line.Replace('"',' ');        //manipulates the data by splitting it up into an array
+			apdata = line.Split(',');
+			//Goes through multiple checks so only relavent data is processed
+			if(apdata[8].Trim().Equals("US", StringComparison.Ordinal) || apdata[8].Trim().Equals("PR", StringComparison.Ordinal) || apdata[8].Trim().Equals("AS", StringComparison.Ordinal))
+			{
+				if(apdata[2].Trim().Equals("small_airport", StringComparison.Ordinal) || apdata[2].Trim().Equals("large_airport", StringComparison.Ordinal))
+				{
+					if(apdata[11].Trim().Equals("no", StringComparison.Ordinal))
+					{
+						//adds a new airport object with all relevant data from airport file
+						apdb.Add(new Airport(Int32.Parse(apdata[0].Trim()), apdata[3].Trim(), Convert.ToDouble(apdata[4].Trim()), Convert.ToDouble(apdata[5].Trim()), 
+									apdata[8].Trim(), apdata[9].Substring(4,2), apdata[10].Trim(), apdata[12].Trim(), 0, " ", 0));
+					}
+				}
+			}
+			line = wr.getLine();
 		}
-    
-    string url_runways = "http://theochem.mercer.edu/csc330/data/runways.csv";
+
+		string url_runways = "http://theochem.mercer.edu/csc330/data/runways.csv";
 		wr = new WebReader(url_runways);        //reads in the runway file
-    line = wr.getLine();    line = wr.getLine();      //skips header line
-    int index = -1;
-    
-    while(line != null)       //iterates throught runway file
-    {
-      line = line.Replace('"',' ');
-      apdata = line.Split(',');
-      index = apdb.FindIndex(x => x.Id == Int32.Parse(apdata[1].Trim()));    //check if runway Id matches any airport Id
-      if(index != -1)
-      {
-          //adds relevant data from runway file to objects created from airport file
-          apdb[index].RW_length = Int32.Parse(apdata[3].Trim());
-          apdb[index].RW_surface = apdata[5].Trim();
-          apdb[index].Lighted = Int32.Parse(apdata[6].Trim());
-      }
-		  line = wr.getLine();
+		line = wr.getLine();    line = wr.getLine();      //skips header line
+		int index = -1;
+
+		while(line != null)       //iterates throught runway file
+		{
+			line = line.Replace('"',' ');
+			apdata = line.Split(',');
+			index = apdb.FindIndex(x => x.Id == Int32.Parse(apdata[1].Trim()));    //check if runway Id matches any airport Id
+			if(index != -1)
+			{
+				//adds relevant data from runway file to objects created from airport file
+				apdb[index].RW_length = Int32.Parse(apdata[3].Trim());
+				apdb[index].RW_surface = apdata[5].Trim();
+				apdb[index].Lighted = Int32.Parse(apdata[6].Trim());
+			}
+			line = wr.getLine();
 		}
-    
-    for(int i = 0; i < apdb.Count; i++)        //displays all airport objects and some of their data
-    {
-      Console.WriteLine(i + " : " + apdb[i].Name + " : " + apdb[i].Country + " : " + apdb[i].State + " : " + apdb[i].RW_length);
-    }
-    return apdb;
-  }
->>>>>>> 36021925e69472740c15c444c9c6530ec5f2a5dc
+
+		for(int i = 0; i < apdb.Count; i++)        //displays all airport objects and some of their data
+		{
+			Console.WriteLine(i + " : " + apdb[i].Name + " : " + apdb[i].Country + " : " + apdb[i].State + " : " + apdb[i].RW_length);
+		}
+		return apdb;
+	}
 
 }
 
