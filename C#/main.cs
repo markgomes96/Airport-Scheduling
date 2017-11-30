70aee7f8af8f3bd22c9d33864e8661c098334b8b
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-11-29 21:33:27 -0500

    Added user input that will look like the example

--- C#/main.cs
+++ C#/main.cs
@@ -20,5 +20,42 @@ public class reader {
         Console.WriteLine("\tdistance: " + p.distance + " miles");
         Console.WriteLine("\tprice: $" + p.price);
     }
+      static public void userInput() {
+            bool flight = true;
+            string choice;
+            Passenger user = new Passenger();
+            Console.WriteLine("Flight Itinerary Builder Program \n");
+            while(flight == true)
+            {
+                  Console.Write("Do you want to add a passenger to your itinerary list (Y/N)? ");
+                  choice = Console.ReadLine();
+                  if(choice == "Y" || choice == "y")  //User wants to add passenger
+                  {
+                        Console.Write("First Name: ");
+                        user.firstname = Console.ReadLine();
+                        Console.Write("Last Name: ");
+                        user.lastname = Console.ReadLine();
+                        Console.Write("Origination City: ");
+                        user.origin.city = Console.ReadLine();
+                        Console.Write("Origination State (two letter code): ");
+                        user.origin.state = Console.ReadLine();
+                        Console.Write("Destination City: ");
+                        user.destination.city = Console.ReadLine();
+                        Console.Write("Destination State (two letter code): ");
+                        user.destination.state = Console.ReadLine();
+                        Console.WriteLine();
+                  }
+                  else if(choice == "N" || choice == "n")  //User done adding passengers
+                  {
+                        flight = false;
+                        Console.WriteLine();
+                        Console.WriteLine("** Generating Itinerary **");
+                  }
+                  else  //User input is wrong
+                  {
+                        Console.WriteLine("\t *** Please Type Y or N ***");
+                  }
+            }
+      }
 }
 
