cff760dd41f1a05a04386eda8eb1f0a9ef98c9e2
Avery Zebell <averyzebell@gmail.com>
2017-12-02 21:56:40 -0500

    Airport comments

--- C#/Airport.cs
+++ C#/Airport.cs
@@ -1,10 +1,18 @@
 using System;
 using System.Collections.Generic;
 
+/*---------------------------------------------------------
+ * The Airport class holds data about its location and
+ * passengers departing from or arriving to the airport. 
+ * 
+ * The 'position' should store the city and state name
+ * it has been found to be nearest to, and the actual 
+ * lat and long that belong to the airport.
+ *--------------------------------------------------------*/
 public class Airport {
 
-	public Location position;
 	public string name;
+	public Location position;
 	public List<Passenger> departing;
 	public List<Passenger> arriving;
 	public bool lighted;
