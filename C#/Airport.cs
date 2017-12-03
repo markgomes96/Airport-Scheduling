d1ee564c662dff735710b5c93a4338440a60a6c9
Avery Zebell <averyzebell@gmail.com>
2017-12-02 19:26:48 -0500

    Airport name property (necessary for output)

--- C#/Airport.cs
+++ C#/Airport.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 public class Airport {
 
 	public Location position;
+	public string name;
 	public List<Passenger> departing;
 	public List<Passenger> arriving;
 	public bool lighted;
 
-	public Airport(Location pos = new Location(), bool lighted = false) {
+	public Airport(string name = null, Location pos = new Location(), bool lighted = false) {
+		this.name = name;
 		this.position = pos;
 		this.departing = new List<Passenger>();
 		this.arriving = new List<Passenger>();
